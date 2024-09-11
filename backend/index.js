dfdc7125c993ee3956795d72930f6b83fdf508af
Saksham <ssaksham952@gmail.com>
2024-09-11 16:45:20 +0530

    edited index.js

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,10 @@ app.use(cors())
 app.use("/auth",authRoute)
 app.use("/products",productRoute)
 
+app.use((req, res, next) => {
+  res.status(404).send("Page Not Found");
+});
+
 app.listen(PORT, async (req, res) => {
   console.log(`Server is running on ${PORT}`)
 });
